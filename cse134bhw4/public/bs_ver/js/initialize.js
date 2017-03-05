10334594029556cd4f38c0de72bf378a74696f01
David Lee <dal039@ucsd.edu>
2017-03-04 20:13:49 -0800

    add firebase initialization

---
+++ cse134bhw4/public/bs_ver/js/initialize.js
@@ -0,0 +1,15 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCan5sw0l48qC_VBhdpdz7vN_hVv3LYo8g",
+  authDomain: "checkmyclass-22c54.firebaseapp.com",
+  databaseURL: "https://checkmyclass-22c54.firebaseio.com",
+  storageBucket: "checkmyclass-22c54.appspot.com",
+  messagingSenderId: "754689393631"
+};
+
+// Initialize Firebase App
+var app = firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = app.database();
