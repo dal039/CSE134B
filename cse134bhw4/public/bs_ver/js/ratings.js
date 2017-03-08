c9510a38c8aaab1e63512501b6a12696b9a5f245
NikAiyer <nick.aiyer@gmail.com>
2017-03-08 12:53:55 -0800

    added rating vuejs

--- cse134bhw4/public/bs_ver/js/ratings.js
+++ cse134bhw4/public/bs_ver/js/ratings.js
@@ -1,49 +1,125 @@
-$(function() {
-	var db = firebase.database()
-	var ref = db.ref('ratings');
+var db = app.database();
 
-	$(document).ready(function() {
-		$('#ta-rating-form').on('submit', function(event) {
-			var helpfulness = $('.help').val();
-			var rating = $('.rating').val();
+var ratingRef = db.ref('/ratings');
 
-			console.log(helpfulness,rating);
-			ref.update({TA_Helpfulness: helpfulness, TA_Rating: rating});
-		});	
-		$('#professor-rating-form').on('submit', function(event) {
+Vue.use(VueFire);
 
-			var difficulty = $('.difficulty').find(":selected").text();
-			var helpfulness = $('.help').val();
-			var rating = $('.rating').val();
+window.addEventListener('load', function() {
+	var ta_vm = new Vue({
+		el: '#ta-rating-form',
+		data: {
+			ta_helpfulness: "",
+			ta_rating: ""
+		},
+  		firebase: {
+		  ratings: ratingRef
+		}, 
+		methods: {
+			rateTA: function () {
+				ratingRef.update({
+					"TA_Helpfulness": this.ta_helpfulness,
+					"TA_Rating": this.ta_rating
+				})
+			}
 
-			console.log(difficulty, helpfulness,rating);
-			ref.update({Professor_Difficulty: difficulty, Professor_Helpfulness: helpfulness, Professor_Rating: rating});
-		});	
-		$('#class-rating-form').on('submit', function(event) {
-			var difficulty = $('.difficulty').find(":selected").text();
-			var usefulness = $('.use').val();
-			var rating = $('.rating').val();
 
-			console.log(difficulty, usefulness,rating);
-			ref.update({Class_Difficulty: difficulty, Class_Usefulness: usefulness, Class_Rating: rating});
-		});	
-
-		ref.on('child_added', function(data) {
-        		var newPost = data.val();
-        		console.log(": " + newPost.author);
-        		console.log("Title: " + newPost.title);
-        		console.log("Previous Post ID: " + prevChildKey);
-    		}
-
-    		function onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
-        		Post changedPost = dataSnapshot.getValue(Post.class);
-        		console.log("The updated post title is: " + changedPost.title);
-    		}
-
-    		function onChildRemoved(DataSnapshot dataSnapshot) {
-        		Post removedPost = dataSnapshot.getValue(Post.class);
-		        console.log("The blog post titled " + removedPost.title + " has been deleted");
-    		}
-		});
+		}
 	});
+
+	var professor_vm = new Vue({
+		el: '#prof-rating-form',
+		data: {
+			prof_difficulty: "",
+			prof_helpfulness: "",
+			prof_rating: ""
+		},
+  		firebase: {
+		  ratings: ratingRef
+		}, 
+		methods: {
+			rateProf: function () {
+				ratingRef.update({
+					"Professor_Difficulty": this.prof_difficulty,
+					"Professor_Helpfulness": this.prof_helpfulness,
+					"Professor_Rating": this.prof_rating
+				})
+			}
+		}
+	});
+
+	var class_vm = new Vue({
+		el: '#class-rating-form',
+		data: {
+			class_difficulty: "",
+			class_usefulness: "",
+			class_rating: ""
+		},
+  		firebase: {
+		  ratings: ratingRef
+		}, 
+		methods: {
+			rateClass: function () {
+				ratingRef.update({
+					"Class_Difficulty": this.class_difficulty,
+					"Class_Usefulness": this.class_usefulness,
+					"Class_Rating": this.class_rating
+				})
+			}
+		}
+	});
+
+
+
 });
+
+
+
+// $(function() {
+// 	var db = firebase.database()
+// 	var ref = db.ref('ratings');
+
+// 	$(document).ready(function() {
+// 		$('#ta-rating-form').on('submit', function(event) {
+// 			var helpfulness = $('.help').val();
+// 			var rating = $('.rating').val();
+
+// 			console.log(helpfulness,rating);
+// 			ref.update({TA_Helpfulness: helpfulness, TA_Rating: rating});
+// 		});	
+// 		$('#professor-rating-form').on('submit', function(event) {
+
+// 			var difficulty = $('.difficulty').find(":selected").text();
+// 			var helpfulness = $('.help').val();
+// 			var rating = $('.rating').val();
+
+// 			console.log(difficulty, helpfulness,rating);
+// 			ref.update({Professor_Difficulty: difficulty, Professor_Helpfulness: helpfulness, Professor_Rating: rating});
+// 		});	
+// 		$('#class-rating-form').on('submit', function(event) {
+// 			var difficulty = $('.difficulty').find(":selected").text();
+// 			var usefulness = $('.use').val();
+// 			var rating = $('.rating').val();
+
+// 			console.log(difficulty, usefulness,rating);
+// 			ref.update({Class_Difficulty: difficulty, Class_Usefulness: usefulness, Class_Rating: rating});
+// 		});	
+
+// 		ref.on('child_added', function(data) {
+//         		var newPost = data.val();
+//         		console.log(": " + newPost.author);
+//         		console.log("Title: " + newPost.title);
+//         		console.log("Previous Post ID: " + prevChildKey);
+//     		}
+
+//     		function onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
+//         		Post changedPost = dataSnapshot.getValue(Post.class);
+//         		console.log("The updated post title is: " + changedPost.title);
+//     		}
+
+//     		function onChildRemoved(DataSnapshot dataSnapshot) {
+//         		Post removedPost = dataSnapshot.getValue(Post.class);
+// 		        console.log("The blog post titled " + removedPost.title + " has been deleted");
+//     		}
+// 		});
+// 	});
+// });
