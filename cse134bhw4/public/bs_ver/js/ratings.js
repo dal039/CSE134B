aa4601c70983f905a22a8b8d7d7bf88296173970
NikAiyer <nick.aiyer@gmail.com>
2017-03-07 23:11:47 -0800

    working on rating

--- cse134bhw4/public/bs_ver/js/ratings.js
+++ cse134bhw4/public/bs_ver/js/ratings.js
@@ -1,28 +1,49 @@
-Vue.use(VueFire);
+$(function() {
+	var db = firebase.database()
+	var ref = db.ref('ratings');
 
+	$(document).ready(function() {
+		$('#ta-rating-form').on('submit', function(event) {
+			var helpfulness = $('.help').val();
+			var rating = $('.rating').val();
 
-var db = firebase.database()
-var ref = db.ref('ratings');
+			console.log(helpfulness,rating);
+			ref.update({TA_Helpfulness: helpfulness, TA_Rating: rating});
+		});	
+		$('#professor-rating-form').on('submit', function(event) {
 
-window.addEventListener('load', function () {
-   var vm = new Vue({
-	  el: ".ratings",
-	  data: {
-	    helpfulness: "",
-	    rating: ""
-	  },
-	  firebase: {
-	    ratings: ref
-	  }, 
-	  methods: {
-	    setRatings: function () {
-    		ref.set({
-    			"helpfulness": this.helpfulness,
-    			"rating": this.rating
-    		})
-    		this.helpfulness= ""
-    		this.rating= ""
-	    }
-	  }
+			var difficulty = $('.difficulty').find(":selected").text();
+			var helpfulness = $('.help').val();
+			var rating = $('.rating').val();
+
+			console.log(difficulty, helpfulness,rating);
+			ref.update({Professor_Difficulty: difficulty, Professor_Helpfulness: helpfulness, Professor_Rating: rating});
+		});	
+		$('#class-rating-form').on('submit', function(event) {
+			var difficulty = $('.difficulty').find(":selected").text();
+			var usefulness = $('.use').val();
+			var rating = $('.rating').val();
+
+			console.log(difficulty, usefulness,rating);
+			ref.update({Class_Difficulty: difficulty, Class_Usefulness: usefulness, Class_Rating: rating});
+		});	
+
+		ref.on('child_added', function(data) {
+        		var newPost = data.val();
+        		console.log(": " + newPost.author);
+        		console.log("Title: " + newPost.title);
+        		console.log("Previous Post ID: " + prevChildKey);
+    		}
+
+    		function onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
+        		Post changedPost = dataSnapshot.getValue(Post.class);
+        		console.log("The updated post title is: " + changedPost.title);
+    		}
+
+    		function onChildRemoved(DataSnapshot dataSnapshot) {
+        		Post removedPost = dataSnapshot.getValue(Post.class);
+		        console.log("The blog post titled " + removedPost.title + " has been deleted");
+    		}
+		});
 	});
-})
+});
